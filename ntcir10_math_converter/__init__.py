846f003a8aa9d643ae43e3447f80c08983b693b9
Vít Novotný <witiko@mail.muni.cz>
2018-05-21 16:27:40 +0200

    Increment the patch version

--- ntcir10_math_converter/__init__.py
+++ ntcir10_math_converter/__init__.py
@@ -8,5 +8,5 @@ from .__main__ import main
 
 
 __author__ = "Vit Novotny"
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 __license__ = "MIT"
--- setup.py
+++ setup.py
@@ -48,5 +48,5 @@ setup(
         "Source": SOURCE_URL,
     },
     url=SOURCE_URL,
-    version="0.1.4",
+    version="0.1.5",
 )
